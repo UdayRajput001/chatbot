90bad1495d9ce6b2b319219e1b5af372b73b351c
UdayRajput001 <132374104+UdayRajput001@users.noreply.github.com>
2024-11-15 12:43:16 +0530

    interactive chatbot

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ app = FastAPI()
 # Enable CORS for all origins (you can specify your allowed domains here)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins. Adjust to your needs for security.
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods (GET, POST, OPTIONS, etc.)
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],  
+    allow_headers=["*"],  
 )
 
 # MongoDB configuration
